f5e9f8425cae1644b1d076fbcabce35cc2b91ab2
“rebba” <rebba@gmail.com>
2021-11-10 14:39:09 +0530

    Initial commit

---
+++ src/app/student/dc-report/dc-report.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, Router } from '@angular/router';
+import { StudentService } from 'src/app/services/student.service';
+
+@Component({
+  selector: 'app-dc-report',
+  templateUrl: './dc-report.component.html',
+  styleUrls: ['./dc-report.component.css']
+})
+export class DcReportComponent implements OnInit {
+
+  report :any;
+
+  constructor(private student:StudentService,private router: Router,private route:ActivatedRoute,private snackbar:MatSnackBar) { }
+
+  ngOnInit(): void {
+    this.getDcReport();
+  }
+
+  getDcReport(){
+    this.student.getDcReportByUserId(this.route.snapshot.params.id,sessionStorage.getItem('userid')).subscribe(
+      (data:any)=>{
+        console.log(data);
+        this.report  = data;
+      }
+    );
+  }
+
+}
